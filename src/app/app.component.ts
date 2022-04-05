aadc9cd479ce7e56b41bb00ce472c7d8283a9b68
Amin <muhammad-karim@hotmail.com>
2022-04-05 18:29:42 +0100

    add rendor books view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+
+interface Book {
+  ISBN: number;
+  Title: string;
+  Author: string;
+}
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myApp-SL';
+  url = '/api/books';
+  books!: Book[];
+
+  constructor(private http: HttpClient){
+  }
+
+  async ngOnInit(){
+    // alert('Richard is cool');
+    const response: any = await this.http.get(this.url).toPromise();
+    console.log(response);
+    if (response) {
+      this.books = response;
+      console.log(this.books);
+    }
+  }
+
 }
